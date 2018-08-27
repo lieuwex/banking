c00f1cb2c4d7dbfd1f878f64b99ec63361343028
Lieuwe Rooijakkers <lieuwerooijakkers@gmail.com>
2018-08-27 20:49:15 +0200

    view: add remove-tag command

--- view/commands.go
+++ view/commands.go
@@ -4,6 +4,7 @@ import "strings"
 
 func (s *ViewState) runCommand(cmd string) {
 	args := strings.Split(cmd, " ")
+	entry := s.table.GetSelected()
 
 	switch args[0] {
 	case "w", "write":
@@ -13,8 +14,19 @@ func (s *ViewState) runCommand(cmd string) {
 		s.app.Stop()
 
 	case "t", "tag":
-		entry := s.table.GetSelected()
 		entry.Tags = append(entry.Tags, args[1])
+	case "rt", "remove-tag":
+		index := -1
+		for i, tag := range entry.Tags {
+			if tag == args[1] {
+				index = i
+				break
+			}
+		}
+
+		if index > -1 {
+			entry.Tags = append(entry.Tags[:index], entry.Tags[index+1:]...)
+		}
 
 	default:
 		panic("unknown command " + cmd)
